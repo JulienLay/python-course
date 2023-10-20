6a516ffcccaccdbb745da4640a063b80dec7e380
Julien Lay <julien-lay@outlook.fr>
2023-10-20 16:14:50 +0200

    caesar

---
+++ course/cesar.py
@@ -0,0 +1,20 @@
+def cesar(number):
+    message = "this is a test !"
+    tr = ""
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    alphabet_changed = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    for i in range(number):
+        first_character = alphabet_changed.pop(0)
+        alphabet_changed.append(first_character)
+
+    for letter in message:
+        try :
+            index_alphabet = alphabet.index(letter.lower())
+        except Exception:
+            tr += letter
+        else :
+            letter_alphabet_changed = alphabet_changed[index_alphabet]
+            tr += letter_alphabet_changed
+    print(tr)
+cesar(1)
