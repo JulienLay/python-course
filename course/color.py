6327f9a025012ba4b20a12b2943c0fdd11594890
Julien Lay <julien-lay@outlook.fr>
2023-10-20 15:08:48 +0200

    color

---
+++ course/color.py
@@ -0,0 +1,18 @@
+import random
+from sty import fg
+
+def random_color_generator():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return [r, g, b]
+
+def color():
+    message = 'This is a message'
+    tr = ''
+    for letter in message:
+        tr += fg(random_color_generator()[0], random_color_generator()[1], random_color_generator()[2]) + letter
+    print(tr)
+    print(fg(255,255,255))
+
+color()
